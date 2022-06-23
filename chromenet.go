1aad18c309b9fefc217f584177d2b107a7461081
sergystepanov <sergystepanov@gmail.com>
2022-06-23 23:16:39 +0300

    Use proper clenup

--- chromenet.go
+++ chromenet.go
@@ -3,18 +3,47 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/chromedp/chromedp"
 )
 
-func chromeURL() (func(w http.ResponseWriter, r *http.Request), context.CancelFunc) {
-	// create context
-	ctx, cancel := chromedp.NewContext(
-		context.Background(),
-		//chromedp.WithDebugf(log.Printf),
+func chromeURL() (func(w http.ResponseWriter, r *http.Request), []func()) {
+	ua := os.Getenv("UA")
+	if ua == "" {
+		ua = defaultUA
+	}
+	log.Printf("UA: %v", ua)
+
+	var cleanup []func()
+
+	dir, err := ioutil.TempDir("", "chrome")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Chrome dir: %v", dir)
+	cleanup = append(cleanup, func() {
+		_ = os.RemoveAll(dir)
+	})
+
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		//chromedp.DisableGPU,
+		chromedp.UserDataDir(dir),
+		chromedp.UserAgent(ua),
 	)
+
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	cleanup = append(cleanup, cancel)
+
+	// also set up a custom logger
+	ctx, cancel := chromedp.NewContext(
+		allocCtx,
+		chromedp.WithLogf(log.Printf),
+	)
+	cleanup = append(cleanup, cancel)
 
 	return func(w http.ResponseWriter, r *http.Request) {
 		// parse POST params
@@ -33,7 +62,8 @@ func chromeURL() (func(w http.ResponseWriter, r *http.Request), context.CancelFu
 		var buf string
 		// capture entire browser viewport, returning png with quality=90
 		if err := chromedp.Run(ctx, getHTML(url, &buf)); err != nil {
-			log.Fatal(err)
+			_, _ = fmt.Fprintf(w, "%s", err)
+			return
 		}
 
 		charset := findCharset(buf)
@@ -41,12 +71,13 @@ func chromeURL() (func(w http.ResponseWriter, r *http.Request), context.CancelFu
 
 		w.Header().Set("Content-Type", "text/html; charset="+charset)
 		_, _ = fmt.Fprintf(w, "%s", buf)
-	}, cancel
+	}, cleanup
 }
 
 func getHTML(url string, body *string) chromedp.Tasks {
 	return chromedp.Tasks{
 		chromedp.Navigate(url),
+		//chromedp.WaitNotPresent(),
 		chromedp.OuterHTML("html", body, chromedp.ByQuery),
 	}
 }
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 )
 
 const (
@@ -22,28 +25,45 @@ func main() {
 	clientType := strings.ToLower(os.Getenv("CLIENT_TYPE"))
 	log.Printf("Client: %v", clientType)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+	h := http.NewServeMux()
+
+	h.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
 		_, _ = fmt.Fprintf(w, messageGoAway)
 	})
-	http.HandleFunc("/s/", func(w http.ResponseWriter, r *http.Request) {
+	h.HandleFunc("/s/", func(w http.ResponseWriter, r *http.Request) {
 		_, _ = fmt.Fprintf(w, "%s", strings.TrimPrefix(r.URL.Path, "/s/"))
 	})
 
 	var handler func(w http.ResponseWriter, r *http.Request)
+	var cancel []func()
 	switch clientType {
 	case "chrome":
-		h, cancel := chromeURL()
-		defer cancel()
-		handler = h
+		handler, cancel = chromeURL()
 	case "go":
 		handler = netURL()
 	case "":
 		handler = netURL()
 	}
 
-	http.HandleFunc("/url", handler)
+	h.HandleFunc("/url", handler)
 
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatalf("Couldn't start the server: %s", err)
+	server := &http.Server{Addr: ":" + port, Handler: h}
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+		<-c
+		log.Printf("Terminate")
+		if cancel != nil && len(cancel) > 0 {
+			for _, f := range cancel {
+				f()
+			}
+		}
+
+		_ = server.Shutdown(context.Background())
+	}()
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("server: %s", err)
 	}
 }
